51363395978af4839fe4d055d088e17a663abb0a
NFoithong <n.foithong1983@gmail.com>
2022-03-26 19:57:46 -0400

    Get all articles route

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const { get } = require('express/lib/response');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-    entended: true
-}));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(bodyParser.urlencoded({
+//     entended: true
+// }));
 app.use(express.static('public'));
 
 //connection
@@ -24,6 +27,18 @@ const atricleSchema = {
 //article model
 const Article = mongoose.model('Article', atricleSchema);
 //TODO
+
+// GET all article
+app.get('/articles', function(req, res) {
+    Article.find(function(err, foundAritcles) {
+        // console.log(foundAritcles);
+        if (!err) {
+            res.send(foundAritcles);
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log('Server started on port 3000');
